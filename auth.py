a8574d125f42d140fe33737041ae3a0ef61b64c9
Gabriel <ramos.gabrielsilva23@gmail.com>
2025-04-02 15:01:33 -0300

    fix: flask app run

--- auth.py
+++ auth.py
@@ -125,4 +125,4 @@ def authorization():
     # return f'Tokens de acesso obtidos e salvos com sucesso!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
